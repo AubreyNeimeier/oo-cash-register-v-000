069a57158984cbe5e48cd12aa25f0b43530bbc77
Aubrey Neimeier <neimeier.1@osu.edu>
2018-01-16 20:54:48 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,19 +13,21 @@ class CashRegister
   end
 
   def add_item(title, price, quantity = 1)
-
-    total += (price * quantity)
-    @last_transaction = (price * quantity)
-    until quantity == 0
-      items << title
-      quantity -= 1
+    @title = title
+    @price = price
+    @quantity = quantity
+    @total += (@price * @quantity)
+    @last_transaction = (@price * @quantity)
+    until @quantity == 0
+      @items << @title
+      @quantity -= 1
     end
   end
 
   def apply_discount
-    if discount != 0
-      discount_percent = discount/100.to_f
-      total = total * (1-discount_percent)
+    if @discount != 0
+      discount_percent = @discount/100.to_f
+      @total = @total * (1-discount_percent)
       "After the discount, the total comes to $#{@total.to_i}."
     else
       "There is no discount to apply."
@@ -33,7 +35,7 @@ class CashRegister
   end
 
   def void_last_transaction
-    total = total - last_transaction
+    total = total - @last_transaction
   end
 
 end
